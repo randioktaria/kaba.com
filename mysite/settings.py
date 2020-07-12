f5fc6993f031dce766592520cd8fa53d4ce61745
Randi Oktaria Rinanda <randioktaria96@gmail.com>
2020-07-12 07:24:19 +0700

    merubah beberapa file dan database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbkaba',
+        'NAME': 'db_kaba_django',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
