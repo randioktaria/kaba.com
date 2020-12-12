eafb7a7962d3b507724a622d457dd53e560b1410
unknown <randioktaria96@gmail.com>
2020-12-12 20:25:18 +0700

    perubahan

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
